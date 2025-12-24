61f2cb300c499b80071f41218a59d50c016bbe91
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2025-12-24 21:42:58 +0530

    add keybinding for going back to the search result

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,5 +10,12 @@ map("i", "jk", "<ESC>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 --
 -- my mappings from here --
+
+
 map("n", "<leader>vv", "<C-w>v", { desc = "Vertical split same buffer" })
 map("n", "<leader>ww", "<cmd>wa<CR>", { desc = "Save all buffers" })
+
+map("n", "<leader>fr", function()
+  require("telescope.builtin").resume()
+end, { desc = "Resume last Telescope picker" })
+
