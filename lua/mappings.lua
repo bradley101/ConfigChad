e94732c8d253ac33f022ddbd96f51c3134699961
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2026-01-07 10:41:04 +0530

    Added some new useful key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -18,4 +18,8 @@ map("n", "<leader>ww", "<cmd>wa<CR>", { desc = "Save all buffers" })
 map("n", "<leader>fr", function()
   require("telescope.builtin").resume()
 end, { desc = "Resume last Telescope picker" })
-
+map("n", "gr", vim.lsp.buf.references, {})
+map("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>",
+  { desc = "LSP References (Telescope)" })
+map("n", "<leader>tn", "<cmd>tabNext<CR>", { desc = "Go to Next Tab" })
+map("n", "<leader>tp", "<cmd>tabPrevious<CR>", { desc = "Go to Previous Tab" })
